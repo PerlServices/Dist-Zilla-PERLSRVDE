141b26db0e1e07546101d4db5ff78449e1589cf6
reneeb <info@perl-services.de>
2022-01-30 08:53:28 +0100

    add stub module for the new namespace

---
+++ lib/Dist/Zilla/PERLSRVDE.pm
@@ -0,0 +1,21 @@
+package Dist::Zilla::PERLSRVDE;
+
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+
+=head1 DESCRIPTION
+
+This is a collection of L<Dist::Zilla> related modules
+used at L<https://perl-services.de|Perl-Services.de>.
+
+=head1 MODULES
+
+=head2 Dist::Zilla::PluginBundle::PERLSRVDE
+
+This is the L<Dist::Zilla::PluginBundle::PERLSRVDE|PluginBundle>
+to include the Plugins we usually use.
+
